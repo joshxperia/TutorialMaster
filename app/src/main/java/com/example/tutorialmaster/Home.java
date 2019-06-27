19e5b6ea13a3b8e0559e50a67ba6dac652b4ecdc
https://github.com/joshxperia <joshtrix8@gmail.com>
2019-06-27 17:27:35 +0300

    streaming phase 3

--- app/src/main/java/com/example/tutorialmaster/Home.java
+++ app/src/main/java/com/example/tutorialmaster/Home.java
@@ -22,26 +22,51 @@ public class Home extends AppCompatActivity {
     private TextView currentTimer;
     private TextView durationTimer;
     private ProgressBar currentProgress;
-
+    private ProgressBar bufferProgress;
+    private boolean isPlaying;
     private Uri videoUri;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        isPlaying =false;
 
         videoView = (VideoView) findViewById(R.id.videoView);
         btn_play_pause = (ImageView) findViewById(R.id.btn_play_pause);
         currentProgress = (ProgressBar) findViewById(R.id.currentProgress);
-        //currentTimer = (TextView) findViewById(R.id.currentTimer);
+        currentTimer = (TextView) findViewById(R.id.currentTimer);
         //durationTimer = (TextView) findViewById(R.id.durationTimer);
+        bufferProgress = (ProgressBar) findViewById(R.id.bufferprogress);
 
-        videoUri = Uri.parse("https://00e9e64bac3d0fceb68036b9b6b3f379428aabd17b44624660-apidata.googleusercontent.com/download/storage/v1/b/webtutorials/o/Programing%2FZero%20to%20Hero%20with%20Python%20Tutorial%20FULL-%20Easy%20Learning%20python%203.4%20from%20begin%20to%20advance.webm?qk=AD5uMEstVZ8QTTplT0_u8UTaZoWauLDMvBwoWOKIT4YAGuY9-w8Dj8N7z9wK4YU0NZErst7Wi2r8QyCNnLnQTLuG-8Kum8CsUCmSgoyajZs_s1EFtSnPfdUyAFeqkVz6VANfuYd295OV78x47piqzgWIHnCmXR-AQacrQNE_7sQXSORaimKHQhvtQasoczufKyO-SBwUQkCP-Al4qatUrW9fI5WtFy7efpQtpBwVQLp3gdhvFvRbwxoDXqNQB6uKinU7w0BvEHLcwyxUMjPCjaipSOr1QrzDqqDYPAUQnL7bPnFwEn3eSEbh97rRbpLLdksIjrRjtGbJQMwaH9Z03HlCx-lr0K-mcdeRI6TjrpXcsT1GMjV81-_C6fesfv5V7Mb7p1MnhVvlFbmeGRbQBdSMh8xWjuuliZi49HmH2-lVnH8bZrgLVYYIEb9X_XM0kAYYyPO0vH2hhD603g9trEcEef3QktVz_2G3bFHsQfiSBe14YDgTe91k68qbzFuNnPnEHh4oxM48ZO1BkL00WxCqMOEovg5sj8QXE2IadAns84qLykeyO8fdMWq2LqoS8c2hdldmxI1Z92ro7tsVD_SM8AtMdLeMWZqhsIdriw3NeNW7uhxOg0WSXMpLAZg0PdOHjKvVLRhtYBzhakP8D_01Xc_f1lt2TwcyXomnoqN2KKKGBOCFXMxSCRpWJO9iAiML1qKiVbgDYhJJPHmySjqvxiQM8L5JwbvtK_gXaD8ZoKRtW9F8GlNfDyaf38RHWFDVzRQNTEJuHPvwjj8jvdKB7ynGDDp-CGi3NQ6IqXpZLKAQwPMj7eJhFGqdb6mXdrixm6OZZQYjM1ej0QITRTWdPGamY3ydPfznJ8jKcV4Zc6A6S-zZL4bDih--q4mHsL0ehtO5Z304UGTkhH158qRbXj7AwvHLqIwnJmBASn4H9WCOQK12FzE");
+        videoUri = Uri.parse("https://firebasestorage.googleapis.com/v0/b/tutorialmaster-dae5a.appspot.com/o/Tech%20Tips%2FReact%20JS%20Crash%20Course.mp4?alt=media&token=ae36374d-f154-4669-972b-0d79d0d1c1f9");
         videoView.setVideoURI(videoUri);
+        videoView.requestFocus();
         videoView.start();
+        isPlaying = true;
+        btn_play_pause.setImageResource(R.drawable.ic_play);
+        btn_play_pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (isPlaying){
+
+                    videoView.pause();
+                    isPlaying =false;
+                    btn_play_pause.setImageResource(R.drawable.ic_play);
+                } else {
+                    videoView.start();
+                    isPlaying =true;
+                    btn_play_pause.setImageResource(R.drawable.ic_play);
+                }
+
+
+            }
+        });
+
 
     }
 
 
-    }
+}
 
